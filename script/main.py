db9fa34654c4e7de01545f897e0686dafa119a72
sperz <morastivenn@gmail.com>
2021-07-03 21:04:29 -0500

    add custom input and print to main.py

--- script/main.py
+++ script/main.py
@@ -1,19 +1,47 @@
+#pytube package
 from pytube import YouTube
-from colorama import Fore, init
+#rich package for terminal
+from rich.console import Console
+from rich.progress import Progress
+from rich.prompt import Prompt
+# time
+import time
 
-init(autoreset=True)
+console = Console()
 
+#Custom header
+
+console.print("""
+███████╗██████╗ ███████╗██████╗ ███████╗
+██╔════╝██╔══██╗██╔════╝██╔══██╗╚══███╔╝
+███████╗██████╔╝█████╗  ██████╔╝  ███╔╝ 
+╚════██║██╔═══╝ ██╔══╝  ██╔══██╗ ███╔╝  
+███████║██║     ███████╗██║  ██║███████╗
+╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝
+""",
+              style="dark_cyan")
+console.print("Welcome!", style="dark_cyan")
+console.print("Made by sperz 2021.", style="dark_cyan")
+
+# declarated path
 path = '/home/sperz/Downloads'
 
-url = [
-    'https://www.youtube.com/watch?v=BH6T3CTuncc',
-    'https://www.youtube.com/watch?v=Qk5M0LHMchM',
-    'https://www.youtube.com/watch?v=zCrsWJSwoi4'
-]
+# start download
 
-for video in url:
-    yt = YouTube(video)
-    video = yt.streams.filter(only_audio=True).first()
-    video.download(path)
+try:
+    link = Prompt.ask("Enter the link of YouTube video",
+                      default="eg. https://www.youtube.com/watch?...")
+    with Progress() as progress:
+        console.print(link)
+        task1 = progress.add_task("[red]Downloading...", total=1000)
+        yt = YouTube(link)
+        video = yt.streams.filter(only_audio=True).first()
+        video.download(path)
 
-print(Fore.GREEN + 'done')
+        while not progress.finished:
+            progress.update(task1, advance=0.9)
+            time.sleep(0.02)
+except KeyboardInterrupt:
+    console.print("Ups! some error with Keyboard.", style="bold red")
+
+console.print("Finish, Back soon :)!", style="dark_cyan")
